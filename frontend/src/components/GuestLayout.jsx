72a53e2fcbde73e38318e06bdc27de91abe68616
Alain NKUSI <alannkusi@gmail.com>
2025-05-25 16:49:26 +0200

    Setting up Layout Structure

---
+++ frontend/src/components/GuestLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const GuestLayout = () => {
+  return (
+    <>
+      <Outlet />      
+    </>
+    
+  )
+}
+
+export default GuestLayout
---
+++ frontend/src/router.jsx
@@ -0,0 +1,51 @@
+import {createBrowserRouter} from 'react-router-dom';
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx';
+import Users from './pages/Users.jsx';
+import NotFound from './pages/NotFound.jsx';
+import DefaultLayout from './components/DefaultLayout.jsx';
+import GuestLayout from './components/GuestLayout.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import { Navigate } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<DefaultLayout/>,
+        children:[
+            {
+                path:'/',
+                element:<Navigate to = '/dashboard'/>
+            },
+            {
+                path:'/dashboard',
+                element:<Dashboard/>
+            },
+            {
+                path:'/users',
+                element:<Users/>
+            },
+        ]
+    },
+    {
+        path:'/',
+        element:<GuestLayout/>,
+        children:[
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/register',
+                element:<Register/>
+            },
+        ]    
+    },      
+    {
+        path:'*',
+        element:<NotFound/>
+    }
+])
+
+
+export default router;
